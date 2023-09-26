51628078d11bb07b6c8c7fb2cfdaa1bac84f285a
vvoo21 <vaneolipa@gmail.com>
2023-09-26 18:56:37 -0500

    Refactor LandingPage component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,24 @@
+import '../styles/landingpage.css'
+import balls from '../assets/balls.png'
+
 const LandingPage = () => {
+  const cvPdfUrl = 'src/assets/CV_VanessaOliveros.pdf';
+
   return (
-    <div>LandingPage</div>
+    <div className="section">
+      <div className='hello-section'>
+        <h1>Hello, I'm Vanessa,<br /><span>software developer.</span></h1>
+        <p>I can help you build a product, feature, or website. Check out some of my work and experience. If you like what you see and have a project that needs coding, do not hesitate to contact me.</p>
+      </div>
+      <img src={balls} alt="balls" className='balls' />
+      <a 
+        href={cvPdfUrl}
+        download="CV_VanessaOliveros.pdf"
+        className='download-button'
+      >
+        Download CV
+      </a>
+    </div>
   )
 }
 
