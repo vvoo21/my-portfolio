0ed6b4950797182401648546421a40d4b91ca6be
vvoo21 <vaneolipa@gmail.com>
2023-10-03 17:19:44 -0500

    Refactor Projects component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,26 +1,21 @@
 import projectsData from "../data/projectsData";
+import '../styles/projects.css';
 
 const Projects = () => {
   return (
     <div className="section">
-      <h2>Projects</h2>
+      <h2 className="navbar-title">Projects</h2>
       <div className="project-list">
         {projectsData.map((project) => (
           <div className="project" key={project.id}>
             <img src={project.image} alt={project.title} style={{width: '100%', height: '100%'}} />
-            <h2>{project.title}</h2>
-            <p>{project.description}</p>
-            <div className="technologies">
-              <strong>Technologies used:</strong>
-              <ul>
-                {project.technologies.map((tech, index) => (
-                  <li key={index}>{tech}</li>
-                ))}
-              </ul>
+            <div className="project-info">
+              <h3 className="project-title">{project.title}</h3>
+              <p className="project-description">{project.description}</p>
             </div>
             <div className="project-links">
-              <a href={project.demoLink} target="_blank" rel="noopener noreferrer">Demo</a>
-              <a href={project.githubLink} target="_blank" rel="noopener noreferrer">GitHub</a>
+              <a href={project.demoLink} className="live-btn" target="_blank" rel="noopener noreferrer">Live preview</a>
+              <a href={project.githubLink} className="github-btn" target="_blank" rel="noopener noreferrer">GitHub</a>
             </div>
           </div>
         ))}
