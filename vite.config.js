c204417b316bef62bb6b25970e94bf8243a0bfa7
vvoo21 <vaneolipa@gmail.com>
2023-10-26 15:24:57 -0500

    Unistall rollup-plugin-copy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    copy({
-      targets: [{ src: 'public', dest: 'dist' }],
-    }),
-  ],
+  plugins: [react()],
 })
